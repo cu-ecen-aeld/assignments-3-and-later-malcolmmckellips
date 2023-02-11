1e94fc75e046866c02019b9bd4d1eef40e0cbf12
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-02-11 13:19:14 -0700

    Add working threading.c/h for assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -8,12 +8,29 @@
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
+#define US_PER_MS (1000)
+
 void* threadfunc(void* thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    if (usleep(thread_func_args->wait_obtain * US_PER_MS))
+    	thread_func_args->thread_complete_success = false;
+    
+    
+    if (pthread_mutex_lock(thread_func_args->mutex))
+    	thread_func_args->thread_complete_success = false;
+    
+    if (usleep(thread_func_args->wait_release * US_PER_MS))
+    	thread_func_args->thread_complete_success = false;
+    
+    if(pthread_mutex_unlock(thread_func_args->mutex))
+    	thread_func_args->thread_complete_success = false;
+    	
     return thread_param;
 }
 
@@ -28,6 +45,22 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+     
+    //Dynamically allocate parameter struct
+    struct thread_data *thread_param = (struct thread_data*) malloc(sizeof(struct thread_data)); 
+    
+    //Assign parameters
+    thread_param->mutex = mutex;
+    thread_param->wait_obtain  = wait_to_obtain_ms;
+    thread_param->wait_release = wait_to_release_ms;
+    thread_param->thread_complete_success = true;
+    
+    //Spawn thread
+    int thread_failure = pthread_create(thread, NULL, threadfunc, (void*)thread_param);
+    
+    if (thread_failure)
+    	return false;
+    else
+    	return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,7 +14,11 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
-
+	
+	pthread_mutex_t *mutex; 
+	int wait_obtain;
+	int wait_release;
+	
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
