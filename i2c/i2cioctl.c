b99f88f51c7bcc1e8e3ae4a513b1f1bd6add4b10
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-04-24 01:32:28 -0600

    add ioctl implementation for burst write/reads to get data from sensor. Might need to also modify write and init function if this doesn't work.

--- i2c/i2cioctl.c
+++ i2c/i2cioctl.c
@@ -8,6 +8,8 @@
 #include <sys/ioctl.h>			//Needed for I2C port
 #include <linux/i2c-dev.h>		//Needed for I2C port
 #include <stdio.h>
+#include <linux/types.h>
+#include <linux/i2c.h>
 
 int file_i2c;
 unsigned char buffer[60] = {0};
@@ -28,36 +30,38 @@ void write_light_sensor(char cmd_code, char data_lsb, char data_msb){
 	printf("Successfully wrote to light sensor\r\n");
 }
 
+//reference: https://forums.raspberrypi.com/viewtopic.php?t=301652
 void read_light_sensor(){
 	printf("Reading light sensor...\r\n");
-	buffer[0] = 0x04;
-	int bytes_to_read = 2; 
-	write(file_i2c, buffer, 1);
-	read(file_i2c, buffer,bytes_to_read);
-	printf("write and read finished\r\n");
-	printf("First byte: %u, Second byte: %u\r\n",(unsigned int)buffer[0],(unsigned int)buffer[1]);
-	/*
-	if (write(file_i2c, buffer, 1) != 1){
-		printf("Failed to select 0x04 register in i2c device read.\r\n");
-		return -1;
-	}
-	//printf("location 0x04 selected in light sensor...\r\n");
-	//int bytes_to_read = 2;
+	
+	struct i2c_rdwr_ioctl_data my_i2c_rdwr;
+	struct i2c_msg i2c_msg_buff[2];
 
-	if (read(file_i2c, buffer,bytes_to_read) != bytes_to_read){
-		printf("Failed to read both bytes in i2c device read.\r\n");
-		return -1;
+	my_i2c_rdwr.nmsgs = 2;
+	my_i2c_rdwr.msgs  = i2c_msg_buff;
+
+	//First i2c message is a write of the register to edit
+	buffer[0] = 0x04; //register
+	i2c_msg_buff[0].addr = 0x10;
+	i2c_msg_buff[0].buf  = buffer;
+	i2c_msg_buff[0].len  = 1; 
+	i2c_msg_buff[0].flags = 0;
+
+	//Next, perform a 2 byte read of ALS
+	i2c_msg_buff[1].addr = 0x10;
+	i2c_msg_buff[1].buf  = buffer;
+	i2c_msg_buff[1].len  = 2; 
+	i2c_msg_buff[1].flags = I2C_M_RD;
+
+	int res = ioctl(file_i2c, I2C_RDWR, &my_i2c_rdwr);
+	if (res < 2){
+		printf("Expected 2 bytes, got %d bytes\r\n", res);
 	}
-	printf("Successfully read both i2c bytes...\r\n");
-	unsigned int raw_value = (buffer[1] << 8) + buffer[0]; //data is in form LSB, MSB
-	printf("Raw Value: %d\r\n",raw_value);
-	float lux = (float)raw_value * 0.0576;
-	*/
-	//return (int)lux;
+	printf("I2C_READ: Byte 1: %c, Byte 2: %c\r\n", buffer[0], buffer[1]);
 }
 
 int main(){
-	printf("I2C program started!\r\n"); 
+	printf("I2C IOCTL program started!\r\n"); 
 
 	if ((file_i2c = open(filename, O_RDWR)) < 0)
 	{
@@ -65,6 +69,7 @@ int main(){
 		return 1;
 	}
 
+	//might interfere with our future ioctls...
 	int addr = 0x10;          //<<<<<The I2C address of the slave
 	if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
 	{
@@ -99,3 +104,4 @@ int main(){
 	}
 	
 }
+
